c67458e212e95b95ec1d17f8a841ceba23e90779
sampod76 <sampodnath76@gmail.com>
2023-12-20 04:46:13 +0600

    cahnge fornt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 "use client";
 import Providers from "@/lib/Providers";
 // import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { StyleProvider } from "@ant-design/cssinjs";
 import ErrorBoundary from "antd/es/alert/ErrorBoundary";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Playfair_Display({ subsets:['cyrillic'],weight:"500"});
 
 // export const metadata: Metadata = {
 //   title: "Next Js Starter",
