fb8ffa13d0ab5a056425bbc500e6386308edf277
JOY-JB <joybarua0058@gmail.com>
2023-12-19 13:55:24 +0600

    image carosel and preview complete

--- src/app/(withlayout)/manage_vehicle/page.tsx
+++ src/app/(withlayout)/manage_vehicle/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Carousel, Modal } from "antd";
+import { Carousel } from "antd";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -25,61 +25,37 @@ const images: ImageType[] = [
 ];
 
 const ManageVehiclePage = () => {
-  const [previewVisible, setPreviewVisible] = useState(false);
   const [selectedImage, setSelectedImage] = useState<ImageType | null>(null);
-
-  const handlePreview = (image: ImageType) => {
-    setSelectedImage(image);
-    setPreviewVisible(true);
-  };
-
-  const handleClosePreview = () => {
-    setPreviewVisible(false);
-  };
 
   return (
     <>
-      <Carousel
-        className="w-1/3  max-h-3 bg-red-800"
-        dots={true}
-        dotPosition="top"
-      >
-        <></>
-        {/* {images.map((image) => (
-          <Image
-            key={image.src}
-            src={image.src}
-            alt={image.alt}
-            width={500}
-            height={500}
-            onClick={() => handlePreview(image)}
-          />
-        ))} */}
-      </Carousel>
-      {previewVisible && selectedImage && (
-        <Modal
-          title={selectedImage?.alt}
-          visible={previewVisible}
-          onCancel={handleClosePreview}
-          footer={null}
-        >
-          (
-          {/* <Image
-            src={selectedImage?.src}
-            alt={selectedImage?.alt}
-            width={500}
-            height={500}
-          /> */}
-          <Image
-            src={selectedImage?.src}
-            alt={selectedImage?.alt}
-            width={100}
-            height={500}
-            style={{ maxWidth: "200px", height: "auto" }}
-          />
-          )
-        </Modal>
-      )}
+      <div className="w-2/4">
+        <Carousel dots={true} autoplay autoplaySpeed={6000}>
+          {images.map((image) => (
+            <Image
+              key={image.src}
+              src={image.src}
+              alt={image.alt}
+              width={500}
+              height={500}
+              className="h-96 object-contain"
+            />
+          ))}
+        </Carousel>
+        <div className="flex ">
+          {images.map((image) => (
+            <Image
+              key={image.src}
+              src={image.src}
+              alt={image.alt}
+              width={100}
+              height={100}
+              className="h-96 object-contain mx-2"
+              onClick={() => console.log(image)}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
