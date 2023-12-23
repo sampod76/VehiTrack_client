d26422e1205353c305f56a8b0f82f51c9dbb7dcc
JOY-JB <joybarua0058@gmail.com>
2023-12-23 13:26:04 +0600

    report summary header done

--- src/app/(withlayout)/super_admin/report/summary/page.tsx
+++ src/app/(withlayout)/super_admin/report/summary/page.tsx
@@ -1,5 +1,50 @@
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormDateRangePicker from "@/components/Forms/FormDateRangePicker";
+import FormSelectField from "@/components/Forms/FormSelectField";
+
 const SummaryPage = () => {
-  return <div>Report summary page</div>;
+  const onSubmit = async (values: any) => {
+    console.log(values);
+  };
+
+  const options = [
+    {
+      label: "Toyota",
+      value: "Toyota",
+    },
+    {
+      label: "BMW",
+      value: "BMW",
+    },
+  ];
+
+  const onChange = (values: any) => {
+    console.log(values);
+  };
+  return (
+    <div>
+      <Form submitHandler={onSubmit}>
+        <div className="flex justify-between">
+          <div className="w-96">
+            <FormSelectField
+              name="vehicle-name"
+              options={options}
+              placeholder="Select vehicle"
+            />
+          </div>
+          <div className="w-96">
+            <FormDateRangePicker
+              name="start-date"
+              onChange={onChange}
+              placeholder={["Start Date", "End Date"]}
+            />
+          </div>
+        </div>
+      </Form>
+    </div>
+  );
 };
 
 export default SummaryPage;
