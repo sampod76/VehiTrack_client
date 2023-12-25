0a18bcbf454c4f315cfbdf1b9efe4cc3bbdee757
JOY-JB <joybarua0058@gmail.com>
2023-12-25 20:28:37 +0600

    unit of measurement page design done

--- src/app/(withlayout)/super_admin/store/unit_of_measurement/page.tsx
+++ src/app/(withlayout)/super_admin/store/unit_of_measurement/page.tsx
@@ -1,11 +1,59 @@
-import React from 'react';
+"use client";
 
-const ExpenseHeadsList = () => {
-    return (
-        <div>
-            <h1>ExpenseHeadsList</h1>
-        </div>
-    );
+import AddEquipmentIn from "@/components/CreateFrom/AddEquipmentIn";
+import ActionBar from "@/components/ui/ActionBar";
+import ModalComponent from "@/components/ui/Modal";
+import UMTable from "@/components/ui/Table";
+import { Button } from "antd";
+import { FaEdit } from "react-icons/fa";
+
+const UnitOfMeasurement = () => {
+  const dataSource = [
+    {
+      id: "123",
+      unitOfMeasurement: "Inches",
+    },
+    {
+      id: "456",
+      unitOfMeasurement: "KG",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "SN",
+      dataIndex: "sn",
+      key: "sn",
+      width: "20%",
+      render: (text: any, record: any, index: number) => {
+        return index + 1;
+      },
+    },
+    {
+      title: "Unit of Measure",
+      dataIndex: "unitOfMeasurement",
+      key: "unitOfMeasurement",
+    },
+    {
+      title: "Action",
+      key: "action",
+      width: "20%",
+      render: (data: any) => {
+        return <Button type="dashed" shape="circle" icon={<FaEdit />} />;
+      },
+    },
+  ];
+
+  return (
+    <div>
+      <ActionBar title="Unit of measurement" inline>
+        <ModalComponent buttonText="Add Unit">
+          <AddEquipmentIn />
+        </ModalComponent>
+      </ActionBar>
+      <UMTable columns={columns} dataSource={dataSource} />
+    </div>
+  );
 };
 
-export default ExpenseHeadsList;
+export default UnitOfMeasurement;
