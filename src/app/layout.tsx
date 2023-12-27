59a0cb5b9a792854aaeba04d6e988e1e2367d5b4
Khairul Alam <khairulislam38@gmail.com>
2023-12-27 22:08:17 +0600

    update some ui design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,15 @@ export default function RootLayout({
 
       <Providers>
         <html lang="en">
-          <body style={{ maxWidth: "1990px" ,marginInline:"auto",boxShadow:
-          "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
-}} className={inter.className}>
+          <body
+            // style={{
+            //   maxWidth: "1990px",
+            //   marginInline: "auto",
+            //   boxShadow:
+            //     "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+            // }}
+            className={inter.className}
+          >
             {children}
           </body>
         </html>
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,13 @@
-import LoadingForDataFetch from "@/components/Utlis/LoadingForDataFetch";
-
+// import LoadingForDataFetch from "@/components/Utlis/LoadingForDataFetch";
+import Loader from "@/components/Utlis/Loader";
 
 const Loading = () => {
-  return <LoadingForDataFetch />;
+  return (
+    <>
+      {/* <LoadingForDataFetch /> */}
+      <Loader className="h-[50vh] flex items-end justify-center" size="large" />
+    </>
+  );
 };
 
 export default Loading;
