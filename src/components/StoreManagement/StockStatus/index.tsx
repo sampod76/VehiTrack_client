7636352e6ce2a03874c167b716dac7ab596937b0
md milon hosen <it000025@gms.com>
2023-12-30 19:12:08 +0600

    Stock Status Work

---
+++ src/components/StoreManagement/StockStatus/index.tsx
@@ -0,0 +1,11 @@
+import MainCard from '@/components/ui/MainCard';
+
+const StockStatus = () => {
+  return (
+    <MainCard title="Stock Status">
+      <p>Stock Status</p>
+    </MainCard>
+  );
+};
+
+export default StockStatus;
