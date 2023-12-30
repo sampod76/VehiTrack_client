b09346e6b95ddf6cb48d8f0cf47a68ce23b2f843
sampod76 <sampodnath76@gmail.com>
2023-12-31 01:37:18 +0600

    add reducith

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,24 @@
-import ContactUs from "@/components/ui/Home/ContactUs";
-import Footer from "@/components/ui/Home/Footer";
-import Header from "@/components/ui/Home/Header";
-import HeroSection from "@/components/ui/Home/HeroSection";
-import OurVehicle from "@/components/ui/Home/OurVehicle";
-import VehicleCategory from "@/components/ui/Home/VehicleCategory";
+'use client'
+import { redirect } from "next/navigation";
 
 const HomePage = () => {
-  return (
-    <section className="max-w-[1990px] mx-auto">
-      <div className="mx-4">
-        <Header />
 
-        <HeroSection />
-        <VehicleCategory/>
-        <OurVehicle />
-        <hr className="my-5 border-2"/>
-        <ContactUs/>
-        <Footer/>
-      </div>
-    </section>
-  );
+  // router.push("/login");
+ 
+  return redirect('/dashboard');
+    // <section className="max-w-[1990px] mx-auto">
+    //   <div className="mx-4">
+    //     <Header />
+
+    //     <HeroSection />
+    //     <VehicleCategory />
+    //     <OurVehicle />
+    //     <hr className="my-5 border-2" />
+    //     <ContactUs />
+    //     <Footer />
+    //   </div>
+    // </section>
+
 };
 
 export default HomePage;
