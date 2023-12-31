14356056fe7284299c40282b0ae73c0072be8c97
sampod76 <sampodnath76@gmail.com>
2024-01-01 03:05:17 +0600

    add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,31 @@
-'use client'
-import { redirect } from "next/navigation";
+"use client";
+import ContactUs from "@/components/ui/Home/ContactUs";
+import Footer from "@/components/ui/Home/Footer";
+import Header from "@/components/ui/Home/Header";
+import HeroSection from "@/components/ui/Home/HeroSection";
+import OurVehicle from "@/components/ui/Home/OurVehicle";
+import TacticalSection from "@/components/ui/Home/TacticalSection";
+import TextCarousel from "@/components/ui/Home/TextCarousel";
+import VehicleCategory from "@/components/ui/Home/VehicleCategory";
 
 const HomePage = () => {
-
   // router.push("/login");
- 
-  return redirect('/dashboard');
-    // <section className="max-w-[1990px] mx-auto">
-    //   <div className="mx-4">
-    //     <Header />
 
-    //     <HeroSection />
-    //     <VehicleCategory />
-    //     <OurVehicle />
-    //     <hr className="my-5 border-2" />
-    //     <ContactUs />
-    //     <Footer />
-    //   </div>
-    // </section>
-
+  return (
+    <section className="">
+      <Header />
+      <div className=" container mx-auto">
+        <HeroSection />
+        <TacticalSection />
+        <TextCarousel />
+        <VehicleCategory />
+        <OurVehicle />
+        <hr className="my-5 border-2" />
+        <ContactUs />
+        <Footer />
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
