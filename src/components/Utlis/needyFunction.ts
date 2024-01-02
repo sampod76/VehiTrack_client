c133fe8015aafb8fbf88226c48bc993e83459594
md milon hosen <it000025@gms.com>
2024-01-02 23:13:38 +0600

    finalize balance sheet

--- src/components/Utlis/needyFunction.ts
+++ src/components/Utlis/needyFunction.ts
@@ -1,5 +1,5 @@
 // summation of array
-export let totalSum = (array: any[], key: string): number => {
+export const totalSum = (array: any[], key: string): number => {
   const initialValue = 0;
   return array.reduce(
     (total, item) => total + parseFloat(item[key] ? item[key] : 0),
