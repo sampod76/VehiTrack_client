0ea0293ce19d2dd06797cbbad04b143d855e6537
md milon hosen <it000025@gms.com>
2024-01-04 03:44:28 +0600

    report complete

--- src/components/Report/AnnualReport/index.tsx
+++ src/components/Report/AnnualReport/index.tsx
@@ -85,13 +85,12 @@ const AnnualReport = () => {
   };
   const onTableChange = (pagination: any, filter: any, sorter: any) => {
     const { order, field } = sorter;
-    // console.log(order, field);
     setSortBy(field as string);
     setSortOrder(order === 'ascend' ? 'asc' : 'desc');
   };
 
   return (
-    <MainCard title="Summary">
+    <MainCard title="Annual Report">
       {/* filter area */}
       <Row gutter={[16, 16]} style={{ marginBottom: 20 }}>
         <Col xs={24} md={8}>
