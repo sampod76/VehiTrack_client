c96c79c3494fcac72546f61b411d9214a0aab6a1
JOY-JB <joybarua0058@gmail.com>
2024-01-05 10:38:08 +0600

    add animation in login page

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -2,6 +2,7 @@
 import { useLoginMutation } from "@/redux/api/auth/authApi";
 import { storeUserInfo } from "@/services/auth.service";
 import { Button, Divider, message } from "antd";
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -9,6 +10,7 @@ import { SubmitHandler } from "react-hook-form";
 import Form from "../Forms/Form";
 import FormInput from "../Forms/FormInput";
 import ButtonLoading from "../ui/Loader/ButtonLoading";
+
 type FormValues = {
   userName: string;
   password: string;
@@ -19,6 +21,18 @@ const LoginPage = () => {
   const [user, setUser] = useState("");
   const [defaultValue, setDefaultValue] = useState({});
   const router = useRouter();
+
+  const pageVariants = {
+    initial: { opacity: 0, y: "-100%" },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: "100%" },
+  };
+
+  const formVariants = {
+    initial: { opacity: 0, x: "-100%" },
+    animate: { opacity: 1, x: 0 },
+  };
+
   const onSubmit: SubmitHandler<FormValues> = async (data) => {
     try {
       const res = await login({ ...data }).unwrap();
@@ -27,16 +41,13 @@ const LoginPage = () => {
         message.success("User logged in successfully");
         storeUserInfo({ accessToken: res?.accessToken });
       } else {
-        message.error("not valid user");
+        message.error("Not a valid user");
       }
     } catch (error: any) {
       message.error(error?.message);
       console.log(error);
     }
   };
-  if (error) {
-    console.log(error);
-  }
 
   useEffect(() => {
     if (user === "super_admin") {
@@ -53,24 +64,51 @@ const LoginPage = () => {
   }, [user]);
 
   return (
-    <div className="relative ">
+    <motion.div
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={pageVariants}
+      className="relative "
+    >
       <img
         src="https://media.giphy.com/media/21QEGwILf5SGDRRHMn/giphy.gif"
         alt=""
         className="absolute inset-0 object-cover w-full h-screen"
       />
-      {/* <div  className="absolute inset-0 object-cover w-full h-screen">
-        <SVG_Web/>
-      </div> */}
-      <div className="relative bg-transparent bg-opacity-75 h-screen ">
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 ">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5 }}
+        className="relative bg-transparent bg-opacity-75 h-screen "
+      >
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 1 }}
+          className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 "
+        >
           <div className="flex flex-col items-center justify-center mt-[15%]">
-            <div className="w-full max-w-xl xl:px-8 xl:w-5/12 ">
-              <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1.5 }}
+              className="w-full max-w-xl xl:px-8 xl:w-5/12 "
+            >
+              <motion.div
+                variants={formVariants}
+                initial="initial"
+                animate="animate"
+                className="bg-white rounded shadow-2xl p-7 sm:p-10"
+              >
                 <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
                   Login Dashboard
                 </h3>
-                <Form submitHandler={onSubmit} defaultValues={defaultValue}>
+                <Form
+                  submitHandler={onSubmit}
+                  defaultValues={defaultValue}
+                  variants={formVariants}
+                >
                   <div>
                     <FormInput
                       name="userName"
@@ -170,12 +208,12 @@ const LoginPage = () => {
                     </div>
                   </div>
                 </Form>
-              </div>
-            </div>
+              </motion.div>
+            </motion.div>
           </div>
-        </div>
-      </div>
-    </div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 };
 
