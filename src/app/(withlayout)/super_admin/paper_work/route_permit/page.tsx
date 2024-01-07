579716659c60963da78a4e34ca1650d166247e66
JOY-JB <joybarua0058@gmail.com>
2024-01-08 00:34:58 +0600

    super admin paper work section responsive done

--- src/app/(withlayout)/super_admin/paper_work/route_permit/page.tsx
+++ src/app/(withlayout)/super_admin/paper_work/route_permit/page.tsx
@@ -15,7 +15,7 @@ import {
   EditOutlined,
   ReloadOutlined,
 } from "@ant-design/icons";
-import { Button, Input, message } from "antd";
+import { Button, Grid, Input, message } from "antd";
 import dayjs from "dayjs";
 import { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
@@ -40,6 +40,9 @@ const RoutePermitPage = () => {
     searchQuery: searchTerm,
     delay: 600,
   });
+
+  const { useBreakpoint } = Grid;
+  const screens = useBreakpoint();
 
   if (!!debouncedTerm) {
     query["searchTerm"] = debouncedTerm;
@@ -111,6 +114,7 @@ const RoutePermitPage = () => {
       render: function (data: any) {
         return data && dayjs(data).format("MMM D, YYYY hh:mm A");
       },
+      responsive: ["xxl"],
       sorter: true,
     },
     {
@@ -149,7 +153,6 @@ const RoutePermitPage = () => {
   ];
 
   const onPaginationChange = (page: number, pageSize: number) => {
-    console.log("Page:", page, "PageSize:", pageSize);
     setPage(page);
     setSize(pageSize);
   };
@@ -167,11 +170,12 @@ const RoutePermitPage = () => {
   };
   return (
     <div className="bg-white border border-blue-200 rounded-lg shadow-md shadow-blue-200 p-5 space-y-3">
-      <ActionBar inline title="Route permit List">
-        <div className="flex items-center gap-2">
+      <ActionBar inline={screens.xs ? false : true} title="Route permit List">
+        <div className="flex items-center justify-between flex-grow gap-2">
           <Input
             // size="large"
             placeholder="Search"
+            value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             // style={{
             //   minWidth: "150px",
